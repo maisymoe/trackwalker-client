93e797b7ff0ada50b6b6c6aed50f0edd6420aefa
Beef <beefers@riseup.net>
2022-09-22 20:02:30 +0100

    [Bugfix] Properly despawn players when not in same map

--- src/lib/handlers/remotePlayer.ts
+++ src/lib/handlers/remotePlayer.ts
@@ -1,28 +1,27 @@
 import { Socket } from "socket.io-client";
-import { spawnPlayerEntity, playerEntities } from "../entityUtils";
+import { spawnPlayerEntity, playerEntities, destroyPlayerEntity } from "../entityUtils";
 import { log } from "../logger";
 
 export default function(socket: Socket) {
     socket.on("playerJoin", async (player: Player) => { 
         log(`${player.username} (${player.id}) joined`);
-
         await spawnPlayerEntity(player);
     });
     socket.on("playerLeave", (player: Player) => { 
         log(`${player.username} (${player.id}) left`);
-        const entity = playerEntities.get(player.id);
-
-        if (entity) {
-            entity?.kill();
-            playerEntities.delete(player.id);
-        }
+        destroyPlayerEntity(player);
     });
     socket.on("playerPositionUpdate", (player: Player, pos: Position) => { 
         // logger.log(`Player position update for ${player.username} (${player.id})\nroom: ${pos.roomName}\nx: ${pos.x}\ny: ${pos.y}\nz: ${pos.z}`);
         const entity = playerEntities.get(player.id);
 
         if (entity) {
-            entity.setPos(pos.x, pos.y, pos.z);
+            // @ts-expect-error
+            if (pos.roomName !== ig.game.mapName) {
+                destroyPlayerEntity(player);
+            } else {
+                entity.setPos(pos.x, pos.y, pos.z);
+            }
         }
     });
     socket.on("playerAnimationUpdate", (player: Player, anim: string, direction: Vec2) => { 
